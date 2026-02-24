743bdf55d9beff38b46dbee116b7e6f1326224f1
sergio-ramon <sergioramon.sr98@hotmail.com>
2026-02-24 10:58:29 -0300

    fix: alteração dos nomes das variáveis do inglês para português

--- src/main/java/com/ramon/guardai/service/BcbService.java
+++ src/main/java/com/ramon/guardai/service/BcbService.java
@@ -16,26 +16,26 @@ import java.util.List;
 public class BcbService {
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    public static BigDecimal getMonthlyDIInterest(String url) {
+    public static BigDecimal pegaTaxaDiMensal(String url) {
         // Cliente HTTP que fará a requisição
-        try (HttpClient client = HttpClient.newHttpClient()) {
+        try (HttpClient cliente = HttpClient.newHttpClient()) {
 
             // Requisição HTTP
-            HttpRequest request = HttpRequest.newBuilder()
+            HttpRequest requisicao = HttpRequest.newBuilder()
                     .GET()
                     .uri(URI.create(url))
                     .timeout(Duration.ofSeconds(10))
                     .build();
 
             // Resposta HTTP recebida por uma Thread separada
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> resposta = cliente.send(requisicao, HttpResponse.BodyHandlers.ofString());
             // Conversão do Json da resposta para SerialData
-            List<SerialData> responseBody = mapper.readValue(response.body(), new TypeReference<>() {});
+            List<SerialData> respostaBody = mapper.readValue(resposta.body(), new TypeReference<>() {});
             // Coleta o valor da taxa diária no atributo value
-            BigDecimal value = responseBody.getFirst().getValue();
+            BigDecimal value = respostaBody.getFirst().getValue();
 
             // Calcula a taxa mensal
-            return calculateMonthlyInterest(value);
+            return calculaTaxaMensal(value);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -43,20 +43,20 @@ public class BcbService {
         }
     }
 
-    private static BigDecimal calculateMonthlyInterest(BigDecimal dataValue) {
+    private static BigDecimal calculaTaxaMensal(BigDecimal valorDado) {
         // Define uma variável com o valor 100 em BigDecimal
         BigDecimal hundred = new BigDecimal("100");
 
         // Pega o valor da taxa diária dividida por 100
-        BigDecimal dailyInterest = dataValue.divide(hundred, RoundingMode.HALF_UP);
+        BigDecimal taxaDiaria = valorDado.divide(hundred, RoundingMode.HALF_UP);
         // Adiciona 1 ao valor da taxa diária
-        dailyInterest = dailyInterest.add(BigDecimal.ONE);
+        taxaDiaria = taxaDiaria.add(BigDecimal.ONE);
         // Eleva a taxa diária a 21 (período mensal de dias úteis)
-        dailyInterest = dailyInterest.pow(21);
+        taxaDiaria = taxaDiaria.pow(21);
         // Subtrai 1 do valor da taxa diári
-        dailyInterest = dailyInterest.subtract(BigDecimal.ONE);
+        taxaDiaria = taxaDiaria.subtract(BigDecimal.ONE);
 
         // Retorna o valor da taxa mensal arredondado com 4 casas decimais
-        return dailyInterest.setScale(4, RoundingMode.HALF_UP);
+        return taxaDiaria.setScale(4, RoundingMode.HALF_UP);
     }
 }
